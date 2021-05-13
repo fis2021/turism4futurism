a81872c7d57155c5b0679b455ff88f2c76ebe685
Sebastian Ciun <sebastian_ciun11@yahoo.com>
2021-05-13 23:05:45 +0300

    new implementations

--- src/main/java/org/loose/fis/sre/Main.java
+++ src/main/java/org/loose/fis/sre/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.loose.fis.sre.model.Destination;
 import org.loose.fis.sre.services.*;
 
 import java.nio.file.Files;
@@ -16,6 +17,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         initDirectory();
         UserService.initDatabase();
+        DestinationService.initDatabase();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         primaryStage.setTitle("Welcome");
         primaryStage.setScene(new Scene(root, 600, 400));
