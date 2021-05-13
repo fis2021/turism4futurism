ef7e7cef7644654ed07ca53bb59d14adf05c2031
Timea-hub <timeaciceu@gmail.com>
2021-05-13 13:02:21 +0300

    home page

--- src/main/java/org/loose/fis/sre/Main.java
+++ src/main/java/org/loose/fis/sre/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.loose.fis.sre.services.BookingService;
 import org.loose.fis.sre.services.HotelService;
 import org.loose.fis.sre.services.UserService;
 
@@ -16,7 +15,6 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         UserService.initDatabase();
         HotelService.initDatabase();
-        BookingService.initDatabase();
         Parent root=FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
--- src/main/java/org/loose/fis/sre/controllers/HomeController.java
+++ src/main/java/org/loose/fis/sre/controllers/HomeController.java
@@ -4,48 +4,52 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
-import javafx.scene.text.Text;
-import javafx.scene.control.TableView;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.collections.ObservableList;
-
-import org.loose.fis.sre.services.HotelService;
-import org.loose.fis.sre.model.Hotels;
 
 import java.io.IOException;
-import java.util.Objects;
 
 
-public class ViewProductsClientController{
-
-    @FXML
-    private Text viewProductsClientMessage;
+public class HomeController{
 
     @FXML
-    private TableView tableView;
+    private Button logoutButton;
 
     @FXML
-    public void initialize() {
-        ObservableList<Hotels> data = tableView.getItems();
-        for (Hotels hotels : HotelService.hotelRepository.find()) {
-            data.add(new Hotels(hotels.getCityName(), hotels.getHotelName(), hotels.getPrice()));
-        }
-        tableView.getSelectionModel().clearSelection();
+    private Button hotelsButton;
+
+    @FXML
+    private Button historyButton;
+
+    public void handlehotels() throws IOException {
+        Stage primary = new Stage();
+        Stage stage = (Stage) hotelsButton.getScene().getWindow();
+        stage.close();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("hotels.fxml"));
+        Scene nextScene = new Scene(root, 600, 400);
+        primary.setScene(nextScene);
+        primary.show();
     }
 
 
-    @FXML
-    public void handleBackButtonAction()
-    {
-        try {
-            Stage stage = (Stage) viewProductsClientMessage.getScene().getWindow();
-            Parent Login = FXMLLoader.load(getClass().getClassLoader().getResource("client.fxml"));
-            Scene scene = new Scene(Login, 900, 600);
-            stage.setScene(scene);
-        }catch(IOException e) {
-            e.printStackTrace();
-        }
+    public void handleHistory() throws IOException {
+        Stage primary = new Stage();
+        Stage stage = (Stage) historyButton.getScene().getWindow();
+        stage.close();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("history.fxml"));
+        Scene nextScene = new Scene(root, 600, 400);
+        primary.setScene(nextScene);
+        primary.show();
+    }
+
+    public void handleLogout() throws IOException {
+        Stage primary = new Stage();
+        Stage stage = (Stage) logoutButton.getScene().getWindow();
+        stage.close();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        Scene nextScene = new Scene(root, 600, 400);
+        primary.setScene(nextScene);
+        primary.show();
     }
 
 }
