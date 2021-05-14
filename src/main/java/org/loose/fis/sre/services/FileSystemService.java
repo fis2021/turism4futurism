01d7f4810c29128a98c08d02168ecbfa04155084
Timea-hub <timeaciceu@gmail.com>
2021-05-14 14:43:53 +0300

    FileSystemService

--- src/main/java/org/loose/fis/sre/services/FileSystemService.java
+++ src/main/java/org/loose/fis/sre/services/FileSystemService.java
@@ -1,54 +1,14 @@
 package org.loose.fis.sre.services;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class FileSystemService{
-    public static String APPLICATION_FOLDER = ".users";
-    public static String HOTELS_FOLDER =".hotels";
-    public static String BOOKINGS_FOLDER =".bookings";
+public class FileSystemService {
+    private static final String APPLICATION_FOLDER = ".registration-example";
     private static final String USER_FOLDER = System.getProperty("user.home");
+    public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
     public static Path getPathToFile(String... path) {
-        return getApplicationHomeFolder().resolve(Paths.get(".", path));
-    }
-
-    public static Path getPathToHotel(String... path) {
-        return getHotelsHomeFolder().resolve(Paths.get(".", path));
-    }
-
-    public static Path getApplicationHomeFolder() {
-        return Paths.get(USER_FOLDER, APPLICATION_FOLDER);
-    }
-
-    public static Path getHotelsHomeFolder(){
-        return Paths.get(USER_FOLDER,HOTELS_FOLDER);
-    }
-
-    public static Path getBookingsHomeFolder() {
-        return Paths.get(USER_FOLDER,BOOKINGS_FOLDER);
-    }
-
-    public static void initDirectory() {
-        Path applicationHomePath = FileSystemService.getApplicationHomeFolder();
-        if (!Files.exists(applicationHomePath))
-            applicationHomePath.toFile().mkdirs();
-    }
-
-    public static void initBookingDirectory() {
-        Path bookingsHomePath = FileSystemService.getBookingsHomeFolder();
-        if (!Files.exists(bookingsHomePath))
-            bookingsHomePath.toFile().mkdirs();
-    }
-
-    public static void initHotelsDirectory(){
-        Path hotelsHomePath = FileSystemService.getHotelsHomeFolder();
-        if (!Files.exists(hotelsHomePath))
-            hotelsHomePath.toFile().mkdirs();
-    }
-
-    public static Path getPathToBooking(String... path) {
-        return getBookingsHomeFolder().resolve(Paths.get(".", path));
+        return APPLICATION_HOME_PATH.resolve(Paths.get(".", path));
     }
 }
