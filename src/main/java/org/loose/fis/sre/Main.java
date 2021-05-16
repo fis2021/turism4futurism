e530eecf6596b1bbfccb1e9f57e3719d12f1f68c
Sebastian Ciun <sebastian_ciun11@yahoo.com>
2021-05-16 19:01:20 +0300

    admin interface

--- src/main/java/org/loose/fis/sre/Main.java
+++ src/main/java/org/loose/fis/sre/Main.java
@@ -5,17 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.loose.fis.sre.model.Destination;
 import org.loose.fis.sre.services.*;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        initDirectory();
         UserService.initDatabase();
         DestinationService.initDatabase();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
@@ -23,13 +22,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
-
-    private void initDirectory() {
-        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
-        if (!Files.exists(applicationHomePath))
-            applicationHomePath.toFile().mkdirs();
-    }
-
 
     public static void main(String[] args) {
         launch(args);
--- src/main/java/org/loose/fis/sre/services/UserService.java
+++ src/main/java/org/loose/fis/sre/services/UserService.java
@@ -10,6 +10,7 @@ import org.loose.fis.sre.model.User;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 import java.util.Objects;
 
 import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
@@ -17,9 +18,11 @@ import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
 public class UserService {
 
     private static ObjectRepository<User> userRepository;
+    private static Nitrite database;
 
     public static void initDatabase() {
-        Nitrite database = Nitrite.builder()
+        FileSystemService.initDirectory();
+        database = Nitrite.builder()
                 .filePath(getPathToFile("registration-example.db").toFile())
                 .openOrCreate("test", "test");
 
@@ -38,7 +41,11 @@ public class UserService {
         }
     }
 
-    private static String encodePassword(String salt, String password) {
+    public static List<User> getAllUsers() {
+        return userRepository.find().toList();
+    }
+
+    public static String encodePassword(String salt, String password) {
         MessageDigest md = getMessageDigest();
         md.update(salt.getBytes(StandardCharsets.UTF_8));
 
@@ -72,4 +79,8 @@ public class UserService {
         throw new UsernameDoesNotExistsException(username);
     }
 
+    public static void close() {
+        userRepository.close();
+        database.close();
+    }
 }
